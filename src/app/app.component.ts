682d4dcb04fde2c3833016b0e829fd2a124a8a7b
Yousef Elsayed <yousef4511@outlook.com>
2024-01-24 22:59:14 +0200

    Add projects section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { BioComponent } from './bio/bio.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, BioComponent, SkillsComponent],
+  imports: [HeroComponent, BioComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
