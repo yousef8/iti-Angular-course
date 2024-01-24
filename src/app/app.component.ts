db44253b749d3a743697c5084d32aad8affdbca5
Yousef Elsayed <yousef4511@outlook.com>
2024-01-24 14:42:49 +0200

    Add bio section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { BioComponent } from './bio/bio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, BioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
