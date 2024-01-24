f8769eb6b1c20a2f0f4f78292247b9fe324ed64e
Yousef Elsayed <yousef4511@outlook.com>
2024-01-24 23:10:30 +0200

    Add footer section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from './hero/hero.component';
 import { BioComponent } from './bio/bio.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, BioComponent, SkillsComponent, ProjectsComponent],
+  imports: [HeroComponent, BioComponent, SkillsComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
