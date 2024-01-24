833ed4781f28a095f4d178b2ce1cbc8dbed071fd
Yousef Elsayed <yousef4511@outlook.com>
2024-01-24 15:16:48 +0200

    Add Skill Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { BioComponent } from './bio/bio.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, BioComponent],
+  imports: [HeroComponent, BioComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
