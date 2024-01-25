28c6a35e4d81f78c47a47c2fa8843e3d99fc9450
Yousef Elsayed <yousef4511@outlook.com>
2024-01-25 13:53:28 +0200

    Finish rendering user cards

--- lab_2/users/src/app/app.component.ts
+++ lab_2/users/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [UsersListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
