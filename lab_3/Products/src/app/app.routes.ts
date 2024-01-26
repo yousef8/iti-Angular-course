ed289d66bf78322b720229572d46f3cae226f496
Yousef Elsayed <yousef4511@outlook.com>
2024-01-26 18:29:46 +0200

    Lab3: Setup basic page routing

--- lab_3/Products/src/app/app.routes.ts
+++ lab_3/Products/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductsComponent } from './products/products.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ProductsComponent
+    },
+    {
+        path: 'login',
+        component: PageUnderConstructionComponent
+    },
+    {
+        path: 'register',
+        component: PageUnderConstructionComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
