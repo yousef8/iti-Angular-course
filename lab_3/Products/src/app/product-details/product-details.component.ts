2941d57f03dd58a2c0419a3976121153f580bbe0
Yousef Elsayed <yousef4511@outlook.com>
2024-01-27 02:35:37 +0200

    Lab3: Handle invalid ids in product details page

--- lab_3/Products/src/app/product-details/product-details.component.ts
+++ lab_3/Products/src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../interfaces/product';
 import * as jsonData from '../../assets/products-list.json';
 import { CurrencyPipe, PercentPipe } from '@angular/common';
 import { RatingComponent } from '../rating/rating.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -15,10 +16,16 @@ export class ProductDetailsComponent {
   @Input() id!: number;
   product?: Product;
 
+  constructor(private router: Router) {
+  }
+
   ngOnInit() {
     const data: any = jsonData;
     const products: Array<Product> = data.default;
     this.product = products.find((element: Product) => element.id == this.id);
+    if (this.product === undefined) {
+      this.router.navigate(['notfound']);
+    }
   }
 
 }
