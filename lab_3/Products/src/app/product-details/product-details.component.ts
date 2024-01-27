de1b3054e113760df6eb6f589586d994c65287f6
Yousef Elsayed <yousef4511@outlook.com>
2024-01-27 02:31:29 +0200

    Lab3: Finish Product Details

--- lab_3/Products/src/app/product-details/product-details.component.ts
+++ lab_3/Products/src/app/product-details/product-details.component.ts
@@ -1,12 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { Product } from '../interfaces/product';
+import * as jsonData from '../../assets/products-list.json';
+import { CurrencyPipe, PercentPipe } from '@angular/common';
+import { RatingComponent } from '../rating/rating.component';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, PercentPipe, RatingComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
   @Input() id!: number;
+  product?: Product;
+
+  ngOnInit() {
+    const data: any = jsonData;
+    const products: Array<Product> = data.default;
+    this.product = products.find((element: Product) => element.id == this.id);
+  }
+
 }
