ed7b66175c93dc00e2ee68ff24e031bf7e437642
Yousef Elsayed <yousef4511@outlook.com>
2024-01-29 23:41:48 +0200

    Lab5: Refactor cart service as possible

--- lab_5/ProductsV3/src/app/services/cart.service.ts
+++ lab_5/ProductsV3/src/app/services/cart.service.ts
@@ -1,54 +1,41 @@
+
 import { Injectable } from '@angular/core';
 import { CartItem } from '../interfaces/cart-item';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, reduce } from 'rxjs';
 import { Product } from '../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cart: BehaviorSubject<Array<CartItem>>;
-  private itemsCount: BehaviorSubject<number>;
-
-  constructor() {
-    this.cart = new BehaviorSubject<Array<CartItem>>([]);
-    this.itemsCount = new BehaviorSubject<number>(0);
-
-    console.log("from cart service ngoninit");
-    this.cart.subscribe((cart) => {
-      let count: number = 0;
-      cart.forEach(item => count += item.quantity);
-      this.itemsCount.next(count);
-    });
-
-  }
+  private cart: BehaviorSubject<Array<CartItem>> = new BehaviorSubject<Array<CartItem>>([]);
+  private itemsCount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   getCart() {
     return this.cart.asObservable();
-  }
-
-  addItem(product: Product) {
-    const cart: Array<CartItem> = this.cart.value;
-
-    if (cart.length === 0) {
-      cart.push({ id: product.id, product: product, quantity: 1 })
-      this.cart.next(cart);
-      return;
-    }
-
-    const cartItemIdx: number = cart.findIndex((item) => item.id === product.id);
-    if (cartItemIdx === -1) {
-      cart.push({ id: product.id, product: product, quantity: 1 })
-      this.cart.next(cart);
-      return;
-    }
-
-    ++cart[cartItemIdx].quantity;
-    this.cart.next(cart);
   }
 
   getItemsCount() {
     return this.itemsCount.asObservable();
   }
 
+  private updateItemsCount() {
+    const cart = this.cart.value;
+    const totalCount = cart.reduce((acc, item) => acc + item.quantity, 0);
+    this.itemsCount.next(totalCount);
+  }
+
+  addItem(product: Product) {
+    const cart: Array<CartItem> = this.cart.value;
+    const existingItemIndex = cart.findIndex((item) => item.id === product.id);
+
+    if (existingItemIndex === -1) {
+      cart.push({ id: product.id, product, quantity: 1 });
+    } else {
+      cart[existingItemIndex].quantity++;
+    }
+
+    this.cart.next([...cart]);
+    this.updateItemsCount();
+  }
 }
